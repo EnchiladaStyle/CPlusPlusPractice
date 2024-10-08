c9cd562aa69d3f943deef0a99f0059366632089e
EnchiladaStyle <jon19073@byui.edu>
2024-10-07 18:39:53 -0600

    practiced linked lists

--- linkedListStruct.cpp
+++ linkedListStruct.cpp
@@ -1,21 +1,92 @@
 #include <iostream>
 
-class Car{
-    private:
-        int vin;
-    public:
-        Car(int _vin){
-            vin = _vin;
-        }
-
-        void showVin(){
-            std::cout<<vin;
-        }
+struct node {
+    int value;
+    node* next;
 };
 
+void addNodeToEnd(node* head, int val){
+
+    node* current = head;
+    while (current->next != nullptr){
+        current = current->next;
+    }
+    node* newNode = new node;
+    newNode->value = val;
+    newNode->next = nullptr;
+
+    current->next = newNode;
+
+}
+
+void showVals(node* head){
+
+    node* current = head;
+    std::cout<<std::endl;
+    while (current != nullptr){
+        std::cout<<current->value;
+        current = current->next;
+    }
+    std::cout<<std::endl;
+
+}
+
+node* reverseLinkedList(node* current, node* prev){
+
+    if (current->next == nullptr){
+        current->next = prev;
+        return current;
+    }
+
+    else{
+        node* newHead = reverseLinkedList(current->next, current);
+        current->next = prev;
+        return newHead;
+    }
+
+}
+
+void removeNthFromEnd(node* head, int n){
+
+    node* dummyNode = new node;
+    dummyNode->next = head;
+
+    node* leftPointer = dummyNode;
+    node* rightPointer = head;
+
+    while (rightPointer != nullptr){
+        if (n > 0){
+            n -= 1;
+        }
+        else{
+            leftPointer = leftPointer->next;
+        }
+        rightPointer = rightPointer->next;
+    }
+
+    leftPointer->next = leftPointer->next->next;
+
+}
 
 int main(){
 
-    Car* mazda = new Car(9);
-    mazda->showVin();
+    node* head = new node;
+    head->value = 1;
+    head->next = nullptr;
+
+    for (int i = 2; i<7; i++){
+        addNodeToEnd(head, i);
+    }
+
+    showVals(head);
+
+    head = reverseLinkedList(head, nullptr);
+
+    showVals(head);
+
+    removeNthFromEnd(head, 3);
+
+    showVals(head);
+
+    delete[] head;
 }
